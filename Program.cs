672ff9428c6cb303429077ce30fd448f997922a3
Eden Jeffrey <edenlukejeffrey@outlook.com>
2024-12-25 18:53:06 +0000

    feat: updated to do list

--- Program.cs
+++ Program.cs
@@ -16,14 +16,16 @@ namespace Unwise
     }
 }
 
-//! TO DO LIST
-//? =BUGS=
+//! =TO DO LIST=
+//? -BUGS-
 //? - Cannot de-select tree node when application is windowed at certain resolutions
 //? - Volume / pitch properties aren't applied from up the tree, only on the AudioContainer being played
 //? - Check for consistency between container name / tag and node name / label etc.
 //=======================================================================================================
+//TODO +FEATURES+
 //TODO - Add sequence containers
 //TODO - Add switch containers
 //TODO - Change pitch-bending algorithmn
 //TODO - Add waveform generation on audio import, and waveform displaying playback bar
 //TODO - Add seek to playback bar
+//TODO - Change container volume control from percentage to decibel offset
